da51e7c65d008fe11cbefa7b7ac126b6c03ae172
Gal Ronen <galronen211@gmail.com>
2023-05-11 14:08:31 +0300

    feat(level-01): stylized side navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,64 @@
 import { useState } from 'react';
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
+import { ThemeProvider, createTheme } from "@mui/material/styles";
 import Header from './components/header/Header';
 import Homepage from './pages/home-page/Homepage';
 import SideNav from './components/side-nav/SideNav';
 import Housepage from './pages/house-page/Housepage';
 
+const theme = createTheme({
+  components: {
+    MuiDrawer: {
+      styleOverrides: {
+        paper: {
+          background: 'black',
+          color: 'white',
+          padding: 20,
+          textAlign: 'center',
+          alignItems: 'center'
+        }
+      }
+    }
+  }
+});
+
 function App() {
   const [sideNavOpened, setSideNavOpened] = useState(false);
 
+  const houses = [
+    {
+      id: 1,
+      name: 'Slytherin',
+      imageUrl: 'https://w7.pngwing.com/pngs/160/467/png-transparent-harry-potter-hogwarts-crest-harry-potter-hogwarts-mystery-harry-potter-hogwarts-mystery-sorting-hat-fictional-universe-of-harry-potter-harry-potter-shield-helga-hufflepuff-potter.png'
+    },
+    {
+      id: 2,
+      name: 'Griffindor',
+      imageUrl: 'https://w7.pngwing.com/pngs/160/467/png-transparent-harry-potter-hogwarts-crest-harry-potter-hogwarts-mystery-harry-potter-hogwarts-mystery-sorting-hat-fictional-universe-of-harry-potter-harry-potter-shield-helga-hufflepuff-potter.png'
+    },
+    {
+      id: 3,
+      name: 'Hufflepuff',
+      imageUrl: 'https://w7.pngwing.com/pngs/160/467/png-transparent-harry-potter-hogwarts-crest-harry-potter-hogwarts-mystery-harry-potter-hogwarts-mystery-sorting-hat-fictional-universe-of-harry-potter-harry-potter-shield-helga-hufflepuff-potter.png'
+    },
+    {
+      id: 4,
+      name: 'Ravenclaw',
+      imageUrl: 'https://w7.pngwing.com/pngs/160/467/png-transparent-harry-potter-hogwarts-crest-harry-potter-hogwarts-mystery-harry-potter-hogwarts-mystery-sorting-hat-fictional-universe-of-harry-potter-harry-potter-shield-helga-hufflepuff-potter.png'
+    }
+  ];
+
   return (
     <div className='app'>
-      <Header setSideNavOpened={setSideNavOpened} />
-      <SideNav sideNavOpened={sideNavOpened} setSideNavOpened={setSideNavOpened}/>
-      <Routes>
-        <Route path='/' element={<Homepage />}></Route>
-        <Route path='/house' element={<Housepage />}></Route>
-      </Routes>
+      <ThemeProvider theme={theme}>
+        <Header setSideNavOpened={setSideNavOpened} />
+        <SideNav sideNavOpened={sideNavOpened} setSideNavOpened={setSideNavOpened} links={houses}/>
+        <Routes>
+          <Route path='/' element={<Homepage />}></Route>
+          <Route path='/house' element={<Housepage />}></Route>
+        </Routes>
+      </ThemeProvider>
     </div>
   );
 }
--- src/components/side-nav/SideNav.tsx
+++ src/components/side-nav/SideNav.tsx
@@ -1,22 +1,42 @@
-import React from 'react'
+import './SideNav.css';
+import React from 'react';
 import Drawer from '@mui/material/Drawer';
-import { Button } from '@mui/material';
+import { Divider, List, ListItem, ListItemButton, ListItemText } from '@mui/material';
+import { House } from '../../models/House';
 
 export type SideNavProps = {
   sideNavOpened: boolean;
   setSideNavOpened: React.Dispatch<React.SetStateAction<boolean>>;
+  links: House[];
 }
 
-function SideNav({sideNavOpened, setSideNavOpened}: SideNavProps) {
+function SideNav({sideNavOpened, setSideNavOpened, links}: SideNavProps) {
   const anchor = 'left';
   const closeValue = false;
+  const title = 'Harry Potter';
+
+  const drawerContent = (
+    <>
+      <span className='app-title'>{title}</span>
+      <Divider></Divider>
+      <List>
+        {links.map(linkObject => 
+          <ListItem key={linkObject.id}>
+            <ListItemButton>
+              <ListItemText>{linkObject.name}</ListItemText>
+            </ListItemButton>
+          </ListItem>
+        ) }
+      </List>
+    </>
+  )
 
   return (
     <Drawer
     anchor={anchor}
     open={sideNavOpened}
     onClose={() => setSideNavOpened(closeValue)}>
-      <Button></Button>
+      {drawerContent}
     </Drawer>
   )
 }
